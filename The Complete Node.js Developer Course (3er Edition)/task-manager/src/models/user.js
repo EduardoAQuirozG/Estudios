00e6f7e3ffe31b16e16728bfb040a17637f19e4e
Eduardo Arturo Quiroz Gallegos <EduardoAQuirozG>
2021-02-11 17:38:03 -0600

    We do a robust user
    1.- We add a new type unique to email, which allows to verify that the email is always unique.
    2.- We add the token, in order to save a new generated token
    3.- We start using the schema mehtods to create personal functions to generate the AuthToken
    4.- We start using the schema statics where we have a static functions in charge of finding the credentials of the user
    5.- We start using a middleware on the userSchema.pre, where we call the save option, and this code iis executed before saving, in this case, hashing the password if its modified
    6.- We add the schema to the User model and export

--- The Complete Node.js Developer Course (3er Edition)/task-manager/src/models/user.js
+++ The Complete Node.js Developer Course (3er Edition)/task-manager/src/models/user.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-const User = mongoose.model('User', {
+// We create the user Schema
+const userSchema = new mongoose.Schema({
     name: {
         type: String, 
         required: true, 
@@ -18,6 +21,7 @@ const User = mongoose.model('User', {
     }, 
     email: {
         type: String, 
+        unique: true,
         required: true, 
         trim: true,
         lowercase: true,
@@ -37,7 +41,54 @@ const User = mongoose.model('User', {
                 throw new Error('The word "password" is included in the password')
             }
         }
-    }
+    }, 
+    tokens: [{
+        token: {
+            type: String, 
+            required: true
+        }
+    }]
 })
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
+
+//We create a static function inside the user schema
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login.')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login.')
+    }
+
+    return user;
+}
+
+//We add a middleware before the save execution that hash the password
+userSchema.pre('save', async function (next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+//We create the model using the schema
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
