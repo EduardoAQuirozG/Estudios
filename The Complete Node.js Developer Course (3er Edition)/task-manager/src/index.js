03ab55dc7efc8b216ac31c28f22a4e158caf82c2
Eduardo Arturo Quiroz Gallegos <EduardoAQuirozG>
2021-02-11 17:30:47 -0600

    We add middleware in order to show that a process can be done between the request and the route handler

--- The Complete Node.js Developer Course (3er Edition)/task-manager/src/index.js
+++ The Complete Node.js Developer Course (3er Edition)/task-manager/src/index.js
@@ -7,6 +7,21 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+
+// Without middleware: new request -> run route handler
+// With middleware: new request -> do something -> run route handler
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.status(400).send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+app.use((req, res, next) => {
+    res.status(503).send('The server is under maintenance.')
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -14,3 +29,18 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+// We use this line to compare a normal password with the hashed password
+//const isMatch = await bcrypt.compare(password, hashedPassword)
+
+const jwt = require('jsonwebtoken')
+
+const myFunction = async () => {
+    const token = jwt.sign({ _id: 'abcde1' }, 'thisismynewcourse', { expiresIn: '7 days'})
+    console.log(token)
+
+    const data = jwt.verify(token, 'thisismynewcourse')
+    console.log(data)
+}
+
+//myFunction()
