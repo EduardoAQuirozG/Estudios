d8bb8bff8d744ce169e963e5203746de0466f97d
Eduardo Arturo Quiroz Gallegos <EduardoAQuirozG>
2021-02-11 18:16:50 -0600

    Update index.js
    We comment the middleware in the index so it doesnt do any process

--- The Complete Node.js Developer Course (3er Edition)/task-manager/src/index.js
+++ The Complete Node.js Developer Course (3er Edition)/task-manager/src/index.js
@@ -18,9 +18,9 @@ const port = process.env.PORT || 3000
 //     }
 // })
 
-app.use((req, res, next) => {
-    res.status(503).send('The server is under maintenance.')
-})
+// app.use((req, res, next) => {
+//     res.status(503).send('The server is under maintenance.')
+// })
 
 app.use(express.json())
 app.use(userRouter)
