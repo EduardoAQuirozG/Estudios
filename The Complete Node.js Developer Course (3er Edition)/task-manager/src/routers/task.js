5008df2d43f65077b2202404f4614568091db8f0
Eduardo Arturo Quiroz Gallegos <EduardoAQuirozG>
2021-02-11 21:46:33 -0600

    Update task.js
    We add the authentication middleware to all the functions and we change that any movement, it has to be validated that the task found also has the id

--- The Complete Node.js Developer Course (3er Edition)/task-manager/src/routers/task.js
+++ The Complete Node.js Developer Course (3er Edition)/task-manager/src/routers/task.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const auth = require('../middleware/auth')
 const router = express.Router()
 const Task = require('../models/task')
 
 // POST for creating a new task
-router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
+router.post('/tasks', auth, async (req, res) => {
+    //const task = new Task(req.body)
+    const task = new Task({
+        ...req.body, 
+        owner: req.user._id
+    }) 
 
     try {
         await task.save()
@@ -15,21 +20,28 @@ router.post('/tasks', async (req, res) => {
 })
 
 // GET for getting a list of tasks
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
+    const _user = req.user
+
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+        // const tasks = await Task.find({ owner: _user._id })
+        // res.send(tasks)
+
+        await _user.populate('tasks').execPopulate()
+        res.send(_user.tasks)
     } catch (error) {
         res.status(500).send(error)
     }
 })
 
 // GET for getting a single task using dynamic link
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
+    const _user = req.user
 
     try {
-        const task = await Task.findById(_id)
+        // const task = await Task.findById(_id)
+        const task = await Task.findOne({ _id, owner: _user._id });
 
         if (!task){
             return res.status(404).send()
@@ -41,7 +53,7 @@ router.get('/tasks/:id', async (req, res) => {
 })
 
 //PATCH for updating the task and validate that everything is correct
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const allowedUpdates = ['description', 'completed']
     
     const _id = req.params.id
@@ -50,17 +62,20 @@ router.patch('/tasks/:id', async (req, res) => {
     const isValidOperation = updates.every(update => allowedUpdates.includes(update))
 
     if (!isValidOperation) {
-        res.status(400).send({ error: 'Invalid update!'})
+        return res.status(400).send({ error: 'Invalid update!'})
     }
+
+    const _user = req.user
+
     try {
-        const task = await Task.findById(_id)
+        // const task = await Task.findById(_id)
+        const task = await Task.findOne({ _id, owner: _user._id })
 
         if (!task) {
-            res.status(404).send()
+            return res.status(404).send()
         }
 
         updates.forEach(update => task[update] = _body[update])
-
         await task.save()
 
         // Este tipo de update no pasa por el schema de save
@@ -76,14 +91,16 @@ router.patch('/tasks/:id', async (req, res) => {
 })
 
 //DELETE for deleting a task from database
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
+    const _user = req.user
 
     try {
-        const task = await Task.findByIdAndDelete(_id)
+        //const task = await Task.findByIdAndDelete(_id)
+        const task = await Task.findOneAndDelete({ _id, owner: _user._id})
 
         if(!task) {
-            res.status(404).send()
+            return res.status(404).send()
         }
 
         res.send(task)
